962a44706875d1d1cca5ee5a03d690f87ee69b1f
4solo <34310696@qq.com>
2015-12-17 00:40:38 -0800

    PP2.2-PP2.3

--- Java/src/PP1_5.java
+++ Java/src/PP1_5.java
@@ -2,8 +2,13 @@
 public class PP1_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Write an application that prints a list of four or five web sites that you enjoy. 
+		//Print both the site name and the URL.
+		System.out.println("Site Name    URL");
+		System.out.println("Google       www.google.com");
+		System.out.println("Amazon       www.amazon.com");
+		System.out.println("Facebook     www.facebook.com");
+		System.out.println("Baidu	     www.baidu.com");
 	}
 
 }
