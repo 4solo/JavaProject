5afb989752dfe166a2d0fdf6004b17d105ff1951
4solo <34310696@qq.com>
2015-12-16 23:53:42 -0800

    PP1.1-PP1.5

---
+++ Java/src/PP1_3.java
@@ -0,0 +1,13 @@
+
+public class PP1_3 {
+
+	public static void main(String[] args) {
+		//name
+		System.out.println("Yi Wang");
+		//birthday
+		System.out.println("03/31");
+		//hobbies
+		System.out.println("Coding");
+	}
+
+}
---
+++ Java/src/PP1_4.java
@@ -0,0 +1,17 @@
+
+public class PP1_4 {
+
+	public static void main(String[] args) {
+		//print Knowledge is Power on one line;
+		System.out.println("Knowledge is Power");
+		//print Knowledge is Power on three lines one per line;
+		System.out.println("Knowledge");
+		System.out.println("   is");
+		System.out.println("  Power");
+		//print Knowledge is Power inside a box made up of the characters = and |
+		System.out.println("====================");
+		System.out.println("|Knowledge is Power|");
+		System.out.println("====================");
+	}
+
+}
